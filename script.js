10a2925cf5640e5bf8b37c12fc8f98975b4bc42c
Alikhan <chort445@gmail.com>
2024-12-07 15:42:34 +0300

    hw

---
+++ script.js
@@ -0,0 +1,36 @@
+//Tapsyrma 1)
+// const users = [];
+
+// for (let i = 0; i < 3; i++) {
+//     const name = prompt(`Adamnyn ${i + 1}-shi atyn engiz:`);
+//     const age = +prompt(`Adamnyn ${i + 1}-shi jasyn engiz:`);
+    
+//     const people = {
+//         name: name,
+//         age: +age
+//     };
+
+//     users.push(people);
+// }
+
+// users.forEach((n, a) => {alert(`people ${a + 1}: Name - ${n.name}, Age - ${n.age}`)});
+
+
+// //Tapsyrma 2)
+// const books = [
+//     { title: "Золотая Орда", author: "Ильяс Есенберлин", year: 1983 },
+//     { title: "Ботагоз", author: "Сабит Муканов", year: 1957 },
+//     { title: "Тернистый путь", author: "Сакен Сейфуллин ", year: 1927 }
+// ];
+
+// const bookTitle = prompt("Kitap atyn engiz:");
+
+// if (books) {
+//     const newYear = prompt(`"${books.title}" Kitap jylyn engiz:`);
+//     books.year = +newYear
+// } else {
+//     alert("Bul kitap massivte tabylmady.");
+// }
+
+// books.forEach(book => { alert(`name: ${b.title}, author: ${b.author}, year: ${b.year}`)});
+// // Bitire almadym
